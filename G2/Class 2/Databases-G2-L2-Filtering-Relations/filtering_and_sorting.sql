3d08027bc0354b8447903b5c9782586cedd3ad77
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-05-26 20:39:25 +0200

    added class code

--- G2/Class 2/Databases-G2-L2-Filtering-Relations/filtering_and_sorting.sql
+++ G2/Class 2/Databases-G2-L2-Filtering-Relations/filtering_and_sorting.sql
@@ -12,3 +12,64 @@ SELECT * FROM [Employee] WHERE FirstName = 'Viktor' OR LastName = 'Trajanov'
 -- Get all employees with first name Viktor or last name Trajanov and order by Date of Birth and Id
 SELECT * FROM [Employee] WHERE FirstName = 'Viktor' OR LastName = 'Trajanov'
 						 ORDER BY DateOfBirth, Id ASC
+
+-- Get all employees with first name Viktor or last name Trajanov
+(SELECT FirstName, LastName from [Employee] WHERE FirstName = 'Viktor')
+UNION
+(SELECT FirstName, LastName from [Employee] WHERE LastName = 'Trajanov')
+
+-- Get all employees with first name Viktor or last name Trajanov (With Duplicate)
+(SELECT FirstName, LastName from [Employee] WHERE FirstName = 'Viktor')
+UNION ALL
+(SELECT FirstName, LastName from [Employee] WHERE LastName = 'Trajanov')
+
+-- Get all employees with first name Viktor and last name Trajanov
+(SELECT Id, FirstName, LastName from [Employee] WHERE FirstName = 'Viktor')
+INTERSECT
+(SELECT FirstName, LastName from [Employee] WHERE LastName = 'Trajanov')
+
+SELECT [Name], [Region] From BusinessEntity
+UNION
+SELECT [Name], [RegionName] FROM Customer
+
+
+
+CREATE TABLE [User] (
+Id INT PRIMARY KEY,
+Age INT DEFAULT (0)
+--CONSTRAINT DF_AGE DEFAULT 0 FOR [AGE]
+)
+
+INSERT INTO [User](Id) VALUES (1)
+
+SELECT * FROM [User]
+
+INSERT INTO [User] VALUES (2, 15)
+
+ALTER TABLE [User]
+ADD CONSTRAINT DF_Age 
+DEFAULT 0 FOR [Age]
+
+CREATE TABLE [Car] (
+Id INT PRIMARY KEY NOT NULL,
+Price INT NOT NULL CHECK ([Price] > 0)
+);
+
+INSERT INTO Car VALUES (1, -1)
+
+ALTER TABLE [Car]
+ADD CONSTRAINT CH_Price 
+CHECK ([Price] > 0 AND Price < 100)
+
+INSERT INTO Car VALUES (1, 111)
+INSERT INTO Car VALUES (1, 99)
+
+CREATE TABLE [Phonebook] (
+Id INT NOT NULL PRIMARY KEY,
+[Name] NVARCHAR(255) NOT NULL,
+PhoneNumber NVARCHAR(15) NOT NULL UNIQUE
+)
+
+SELECT * FROM [Phonebook];
+INSERT INTO [Phonebook] VALUES (1, 'Miki', '078537928')
+INSERT INTO [Phonebook] VALUES (2, 'Bojan', '078537923')
