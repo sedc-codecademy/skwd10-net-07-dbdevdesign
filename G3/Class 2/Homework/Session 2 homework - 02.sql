106e9474faded1519f2bcdf07bf9a8089dd1c293
Martin <panovski.martin93@gmail.com>
2022-05-31 13:50:11 +0200

    Homework
    Homework added

---
+++ G3/Class 2/Homework/Session 2 homework - 02.sql
@@ -0,0 +1,7 @@
+/*
+
+List all BusinessEntity Names and Customer Names in single result set with duplicates
+List all regions where some BusinessEntity is based, or some Customer is based. Remove duplicates
+List all regions where we have BusinessEntities and Customers in the same time
+
+*/
---
+++ G3/Class 2/Homework/Session 2 homework - 03.sql
@@ -0,0 +1,7 @@
+/*
+
+Change Products table always to insert value 1 in price column if no price is provided on insert
+Change Products table to prevent inserting Price that will more than 2x bigger then the cost price
+Change Products table to guarantee unique names across the products
+
+*/
