09db21676e6f5dd081239e2d7ce32b6bb06054c9
Martin <panovski.martin93@gmail.com>
2022-06-02 12:50:48 +0200

    Homewrok
    Class 4 homework added

---
+++ G3/Class 4/Homework/Session 4 - Homework.sql
@@ -0,0 +1,14 @@
+/*
+Calculate the total amount on all orders in the system
+Calculate the total amount per BusinessEntity on all orders in the system
+Calculate the total amount per BusinessEntity on all orders in the system 
+from Customers with ID < 20
+Find the Maximal Order amount, and the Average Order amount 
+per BusinessEntity on all orders in the system
+
+
+
+
+HINT: When 'per' is part of the requirement think of grouping the dataset by something!!!
+
+*/
